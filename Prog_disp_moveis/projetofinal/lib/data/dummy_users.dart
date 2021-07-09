3d59caee532ddf7c1cfef23f3629cefd4a0617e5
daianasousa <69603630+daianasousa@users.noreply.github.com>
2021-07-09 20:11:50 -0300

    Projeto Final

---
+++ Prog_disp_moveis/projetofinal/lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:projetofinal/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'João',
+    email: 'joao@alunos.com.br',
+    matricula: '2020111mtds0451',
+    avatarUrl: 'https://static.vecteezy.com/ti/vetor-gratis/p1/2275847-avatar-masculino-perfil-icone-de-homem-caucasiano-sorridente-vetor.jpg'
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Diana',
+    email: 'Diana@alunos.com.br',
+    matricula: '2020111mtds0234',
+    avatarUrl: 'https://static.vecteezy.com/ti/vetor-gratis/p3/2275818-avatar-feminino-mulher-perfil-icone-para-rede-vetor.jpg'
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Felipe',
+    email: 'felipe@zymail.com.br',
+    matricula: '2020111mtds0851',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2021/07/03/10/54/naruto-6383640_1280.jpg'
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Bruna',
+    email: 'bruna@abcmail.com',
+    matricula: '2020111mtds3443',
+    avatarUrl: 'https://i.pinimg.com/564x/f9/df/c1/f9dfc118f05146a5e42ec925c7dd7521.jpg',
+  )
+};
